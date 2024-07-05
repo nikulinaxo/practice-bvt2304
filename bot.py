b1f48e8f68e61b1cc64e4e3147db6ff4e4d50736
Roberts Vesīks <xo.nikulina.xo@gmail.com>
2024-07-05 20:09:31 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,132 @@
+import asyncio
+import logging
+import sys
+
+from aiogram import Bot, Dispatcher, html, F, Router
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.fsm.context import FSMContext
+from aiogram.filters import CommandStart, Command
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+
+import vacancies
+
+TOKEN = '7471656640:AAEGx0xXM_Q4-Ms0wITshLqMydMMfOY3NIA'
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: Message):
+    await message.answer(f"Привет, {message.from_user.first_name}!"
+                         '\n\n'
+                         f'Пример для поиска вакансий:\n{html.bold('/search Python разработчик Москва')}')
+
+
+class Form(StatesGroup):
+    job = State()
+    sch = State()
+    exp = State()
+
+data = []
+
+hiddenData = []
+
+@dp.message(Command('search'))
+async def search(message: Message, state: FSMContext):
+
+    query = message.text[8::]
+
+    data.append(query)
+    hiddenData.append(query)
+
+    schs = []
+
+    schs.append([KeyboardButton(text='Полный день')])
+    schs.append([KeyboardButton(text='Сменный график')])
+    schs.append([KeyboardButton(text='Гибкий график')])
+    schs.append([KeyboardButton(text='Удаленно')])
+    schs.append([KeyboardButton(text='Вахта')])
+
+
+
+    await message.answer(f'🔍 Ищем {query}\nУкажите график работы', reply_markup=ReplyKeyboardMarkup(keyboard=schs, one_time_keyboard=True, resize_keyboard=True))
+    await state.set_state(Form.sch)
+
+
+@dp.message(F.text, Form.sch)
+async def getExp(message: Message, state: FSMContext):
+
+    data.append(message.text)
+
+    exp = []
+
+    exp.append([KeyboardButton(text='Без опыта')])
+    exp.append([KeyboardButton(text='От года до трех')])
+    exp.append([KeyboardButton(text='От трех до шести')])
+    exp.append([KeyboardButton(text='Более шести')])
+
+    await message.answer(f'И требования к опыту...', reply_markup=ReplyKeyboardMarkup(keyboard=exp, one_time_keyboard=True, resize_keyboard=True))
+    await state.set_state(Form.exp)
+
+@dp.message(F.text, Form.exp)
+async def final(message: Message, state: FSMContext):
+
+    data.append(message.text)
+
+    await message.answer(f"🔍 Ищем вакансии\n"
+                         f"{data[0]}\n"
+                         f"{data[1]}\n"
+                         f"{data[2]}")
+
+    await state.clear()
+
+    match data[1]:
+        case 'Полный день':
+            data[1] = 'fullDay'
+        case 'Сменный график':
+            data[1] = 'shift'
+        case 'Гибкий график':
+            data[1] = 'flexible'
+        case 'Удаленно':
+            data[1] = 'remote'
+        case 'Вахта':
+            data[1] = 'flyInFlyOut'
+
+    match data[2]:
+        case 'Без опыта':
+            data[2] = 'noExperience'
+        case 'От года до трех':
+            data[2] = 'between1And3'
+        case 'От трех до шести':
+            data[2] = 'between3And6'
+        case 'Более шести':
+            data[2] = 'moreThan6'
+
+    for vac in vacancies.get(data[0], data[2], data[1]):
+
+        btns = []
+
+        btns.append([InlineKeyboardButton(text="Подробнее...", url=vac['url'])])
+
+
+
+        await message.answer(html.bold(vac['name']) +
+                             f'\n{html.italic('в г.' + vac['area'])}\n\nТребования: \n' +
+                             html.code(vac['req']) +
+                             f'\n\nЗарплата: {html.bold(vac['sal'])}', reply_markup=InlineKeyboardMarkup(inline_keyboard=btns))
+
+    data.clear()
+
+
+async def main():
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
---
+++ vacancies.py
@@ -0,0 +1,66 @@
+import requests
+from currency_symbols import CurrencySymbols
+
+
+url = "https://api.hh.ru/vacancies"
+
+headers = {
+    "User-Agent": "Mozilla / 5.0 (Windows NT 10.0; Win64; x64) AppleWebKit"
+}
+
+def get(x, e, s):
+
+    params = {
+        "text": x,
+        "per_page": 5,
+        "experience": e,
+        "schedule": s
+    }
+
+    vacancies = requests.get(url, headers=headers, params=params)
+
+    vacancies = vacancies.json()
+
+
+    items = vacancies['items']
+
+    vacancies = []
+
+    for item in items:
+
+        if item['salary'] == None:
+            salary = "Не указано"
+        elif item['salary']['from'] == None:
+            currency = str(item['salary']['currency'])
+            if currency == "RUR":
+                currency = 'RUB'
+            salary = "До " + str(item['salary']['to']) + str(CurrencySymbols.get_symbol(currency))
+        elif item['salary']['to'] == None:
+            currency = str(item['salary']['currency'])
+            if currency == "RUR":
+                currency = 'RUB'
+            salary = "От " + str(item['salary']['from']) + str(CurrencySymbols.get_symbol(currency))
+        else:
+            currency = str(item['salary']['currency'])
+            if currency == "RUR":
+                currency = 'RUB'
+            salary = "От " + str(item['salary']['from']) + str(CurrencySymbols.get_symbol(currency)) + " до " + str(
+                item['salary']['to']) + str(CurrencySymbols.get_symbol(currency))
+
+        requirement = str(item['snippet']['requirement'])
+
+        if "highlighttext" in requirement:
+            requirement = requirement.replace("<highlighttext>", "")
+            requirement = requirement.replace("</highlighttext>", "")
+
+        vacancy = {
+            "name": item['name'],
+            "area": item['area']['name'],
+            "sal": salary,
+            "url": item['alternate_url'],
+            "req": requirement
+        }
+
+        vacancies.append(vacancy)
+
+    return vacancies
